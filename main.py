c1a9376c2323cf932f68c367233f8b6368a0956a
G00Z_G00Z <edygomezgg4@gmail.com>
2022-09-19 17:19:31 -0500

    Separated important columns

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pandas as pd
 
 
+
 dataset_path = "./dataset/Housing.csv"
 
 # Leer la tabla
 table = pd.read_csv(dataset_path)
 
+
+# Obtener X y Y
+columnYname = "price"
+
 columnas = ["area", "bedrooms", "bathrooms", "price"]
 
-table2 = table.loc[:, columnas]
+featuresColNames = filter(lambda col : col != columnYname, columnas)
+yColName = filter(lambda col : col == columnYname, columnas)
+
+X = table.loc[:, featuresColNames]
+Y = table.loc[:, yColName]
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state = 0, train_size = 0.70)
 
 
-
-print(table2)
 
